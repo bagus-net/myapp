3e2016f5c2fd4e4cb4bc2c11eb47e7b7461c54d5
bagus-net <bagussetyawan551@gmail.com>
2024-01-27 21:16:06 +0700

    widget & iconbutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.cyan,
+          primarySwatch: Colors.blue,
         ),
         home: Scaffold(
             appBar: AppBar(
               title: Text("Sabla"),
               backgroundColor: Colors.red,
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.mail), onPressed: () {})
+              ],
             ),
             body: SafeArea(
               child: Text("flutter pertama jon"),
